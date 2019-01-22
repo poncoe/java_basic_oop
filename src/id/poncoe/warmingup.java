e93f2853f26f1273972877b5e6cf604b8d966b43
poncoe <poncoe4mac@gmail.com>
2019-01-23 01:54:06 +0700

    meow commit

---
+++ src/id/poncoe/warmingup.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.poncoe;
+
+/**
+ *
+ * @author poncoe
+ */
+public class warmingup {
+    
+    public static void main(String[] args) {
+        
+        // Run Mod Material
+        
+//      System.out.println("Masukan Nama Anda : ");
+//      System.out.println("");
+//      Scanner in = new Scanner(System.in);
+//      String nama = in.next();
+//      System.out.println("Nama Anda Adalah "+nama);
+	
+	for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < i; j++) {
+            System.out.print("*");
+            
+        }
+        System.out.println("");
+        
+    }
+}
+
+}
