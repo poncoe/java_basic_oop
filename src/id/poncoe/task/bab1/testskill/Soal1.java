5747629cf1b3d9dafa776480db98c4280bce2e7d
poncoe <poncoe4mac@gmail.com>
2019-02-01 00:05:23 +0700

    added test skill 1 & 2 last test

---
+++ src/id/poncoe/task/bab1/testskill/Soal1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.poncoe.task.bab1.testskill;
+
+/**
+ *
+ * @author Poncoe
+ */
+public class Soal1 {
+    
+    public static void main (String[] agrs){
+        
+        int angka, keluaran;
+        angka = 2;
+        keluaran = angka*angka*angka*angka*angka;
+        
+        System.out.println("Outputnya : +"+keluaran);
+        
+    }
+    
+}
---
+++ src/id/poncoe/task/bab1/testskill/Soal2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.poncoe.task.bab1.testskill;
+
+/**
+ *
+ * @author Poncoe
+ */
+public class Soal2 {
+    
+    public static void main (String[] args){
+        
+        int bil1, bil2, bil3;
+        bil1 = 12;
+        bil2 = 10;
+        bil3 = 14;
+        
+        System.out.println(bil1+bil2+bil3);
+        
+    }
+    
+}
