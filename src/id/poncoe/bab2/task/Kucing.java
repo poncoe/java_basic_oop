e069a1ac4507a5e53f74ece6b0a7657f789ce755
poncoe <poncoe4mac@gmail.com>
2019-02-07 11:28:01 +0700

    Belajar Enkapsulasi Objek Kucing

---
+++ src/id/poncoe/bab2/task/Kucing.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.poncoe.bab2.task;
+
+/**
+ *
+ * @author poncoe
+ */
+public class Kucing {
+    
+    public String Nama;
+        public String Kelamin;
+        public String Warna;
+        private int umurkucing;
+            
+    public void setUmur (int umur){
+        umurkucing=umur;
+    }
+    public int getUmur(){
+        return umurkucing;
+    }
+}
