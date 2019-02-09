0b6eeeea88251d105bac3c47b6cb769cfdb42ec6
poncoe <poncoe4mac@gmail.com>
2019-02-09 20:21:43 +0700

    Belajar Enkapsulasi Point di Package Test Skill

---
+++ src/id/poncoe/bab2/testskill/MainEncapsulation02.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.poncoe.bab2.testskill;
+
+/**
+ *
+ * @author PRAKTIKAN
+ */
+public class MainEncapsulation02 {
+    
+    
+        
+        Point p = new Point();
+        Point p1 = new Point();
+        Point p2 = new Point();
+        int x1 = 2;
+        int y1 = 3;
+        int x2 = 7;
+        int y2 = -1;      
+        //p.addKordinat(x1, y1, x2, y2);
+        //p.addKoordinat(p1,p2);
+        //System.out.println("Hasil Kordinat "+p1.x+","+p1.y);
+    
+}
