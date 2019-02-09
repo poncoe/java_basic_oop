b7647271b4ae5f8d19ffddc9633bd046bec0206d
poncoe <poncoe4mac@gmail.com>
2019-02-09 20:21:07 +0700

    Belajar Enkapsulasi Point di Package Test Skill

---
+++ src/id/poncoe/bab2/testskill/MainEncapsulation01.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.poncoe.bab2.testskill;
+
+/**
+ *
+ * @author PRAKTIKAN
+ */
+public class MainEncapsulation01 {
+    
+    public static void main(String[] agrs) {
+        
+        Point p1 = new Point();
+        p1.x = 25;
+        p1.y = 7;
+        
+        Point p2 = new Point();
+        p2.x = 32;
+        p2.y = -17;
+        
+        Point p3 = p2;
+        
+        p3 = p1;
+        
+        System.out.println("Isi p 1 = "+p1.x+","+p1.y);
+        System.out.println("");
+        System.out.println("Isi p 2 = "+p2.x+","+p2.y);
+        System.out.println("");
+        System.out.println("Isi p 3 = "+p3.x+","+p3.y);
+        System.out.println("");
+        System.out.println("Isi p 3 = "+p3.x+","+p3.y);
+        
+        
+    }
+    
+}
---
+++ src/id/poncoe/bab2/testskill/Point.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.poncoe.bab2.testskill;
+
+/**
+ *
+ * @author PRAKTIKAN
+ */
+public class Point {
+        int x,y;
+        
+        void addKordinat (int x1, int y1, int x2, int y2){
+            x1 = x1 + x2;
+            y1 = y1 + y2;
+        }
+        
+        void addKoordinat(Point p1, Point p2) {
+            p1.x = p1.x + p2.x;
+            p1.y = p1.y + p2.y;
+    	}
+
+    
+    public Point(){
+        //int x,y;
+    }
+    
+}
