f9f8891ca69e426ef0c80e7659d339672838d723
poncoe <poncoe4mac@gmail.com>
2019-02-12 20:30:52 +0700

    Improve Student di Package Test Skill

--- src/id/poncoe/bab2/testskill/StudentTestSkill.java
+++ src/id/poncoe/bab2/testskill/StudentTestSkill.java
@@ -9,27 +9,27 @@ package id.poncoe.bab2.testskill;
  *
  * @author PRAKTIKAN
  */
-public class Student {
+public class StudentTestSkill {
     
     String name;
     long studentId;
     Boolean isMale;
     double grade;
     
-    public Student(String namee, long studentIdd, boolean isMalee, double gradee){
+    public StudentTestSkill(String namee, long studentIdd, boolean isMalee, double gradee){
         this.name=namee;
         this.studentId=studentIdd;
         this.isMale=isMalee;
         this.grade=gradee;
     }
     
-    public Student(String nameee, long studentIddd, boolean isMaleee){
+    public StudentTestSkill(String nameee, long studentIddd, boolean isMaleee){
         this.name=nameee;
         this.studentId=studentIddd;
         this.isMale=isMaleee;
     }
     
-    public Student(String nameeee, long studentIdddd){
+    public StudentTestSkill(String nameeee, long studentIdddd){
         this.name=nameeee;
         this.studentId=studentIdddd;
     }
@@ -46,9 +46,9 @@ public class Student {
     }
     
     public static void main (String[] agrs){
-        Student s1 = new Student("Max",321032,true,3.7);
-        Student s2 = new Student("Anny",23331,true);
-        Student s3 = new Student("Bobby",22122);
+        StudentTestSkill s1 = new StudentTestSkill("Max",321032,true,3.7);
+        StudentTestSkill s2 = new StudentTestSkill("Anny",23331,true);
+        StudentTestSkill s3 = new StudentTestSkill("Bobby",22122);
         System.out.println("Student 1");
         s1.display();
         System.out.println("");
