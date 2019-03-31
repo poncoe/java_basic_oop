12396d47925fd8f644e4d2b150da79741b21fd4e
poncoe <poncoe4mac@gmail.com>
2019-03-31 11:52:09 +0700

    Added Main Animal

---
+++ src/id/poncoe/bab5/testskill/DuckMain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.poncoe.bab5.testskill;
+
+/**
+ *
+ * @author Poncoe
+ */
+public class DuckMain {
+    public static void main(String[]args){
+        Duck d1 = new Duck();
+        System.out.println("The Number of Duck "+Duck.duckCount);
+        Duck d2 = new Duck();
+        System.out.println("The Number of Duck "+Duck.duckCount);
+        Duck d3 = new Duck();
+        System.out.println("The Number of Duck "+Duck.duckCount);
+    }
+    
+}
