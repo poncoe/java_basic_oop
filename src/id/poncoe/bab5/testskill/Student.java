e6832a7377e80cf7bf9ecf34c89479c9880bbcfe
poncoe <poncoe4mac@gmail.com>
2019-03-31 11:52:58 +0700

    Added Student Class

---
+++ src/id/poncoe/bab5/testskill/Student.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.poncoe.bab5.testskill;
+
+/**
+ *
+ * @author Poncoe
+ */
+public class Student {
+    private String schoolName;
+    
+    public void setSchoolName(String schoolName){
+        this.schoolName = schoolName;
+    }
+    
+    public String getSchoolName(){
+        return schoolName;
+    }
+    
+}
