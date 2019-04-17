6e397cb10bdd3eda1b36b75469822790298c5a54
poncoe <poncoe4mac@gmail.com>
2019-04-18 00:54:58 +0700

    Added main in bab 8 testskill

---
+++ src/id/poncoe/bab8/testskill/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.poncoe.bab8.testskill;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author poncoe
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       List<User> list = new ArrayList<>();
+       
+    }
+    
+}
