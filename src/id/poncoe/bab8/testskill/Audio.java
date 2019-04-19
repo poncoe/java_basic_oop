a4f3cd2c4fd13a4ef4023e50f8b36e7c1e473dbb
poncoe <poncoe4mac@gmail.com>
2019-04-20 00:56:34 +0700

    Added Audio in bab 8 testskill

---
+++ src/id/poncoe/bab8/testskill/Audio.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.poncoe.bab8.testskill;
+
+/**
+ *
+ * @author poncoe
+ */
+public class Audio {
+    private int bitRate = 256;
+    
+    public void Audio(int size){
+        this.bitRate = size;
+    }
+    
+    public void setBitRate(){
+        this.bitRate = 256;
+    }
+    
+    public int getBitRate(){
+        return bitRate;
+    }
+    
+    public void Open(){
+        
+    }
+    
+    public void download(){
+        
+    }
+    
+    
+}
